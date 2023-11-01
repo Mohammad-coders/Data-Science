722f59e02d6292ee7e09116b6485a348efa36a8f
Mohammad Bin Harun <79358840+Mohammad-coders@users.noreply.github.com>
2023-11-01 15:15:47 +0600

    Update Basic Syntex.R

--- Basic Syntex.R
+++ Basic Syntex.R
@@ -5,6 +5,7 @@
 sqrt(10)
 sqrt(4)
 pi
+
 #Basic Data Types 
 x<-3.1416
 x
@@ -29,20 +30,26 @@ print(k)
 f=-k
 print(f)
 abs(f)
+
 #return to the round value
 ceiling(f)
+
 #return to downward its nearest value
 floor(f)
+
 #character
 tolower(s)
 toupper(s)
 strsplit(s,split = "")
 sum(x,y)
+
 #find the length of string 
 nchar(s)
+
 # to merge or concatenate two string 
 t<- "tO all"
 paste(s,t)
+
 # multiline string 
 str<- "Hello I am Mohammad. I am a student.I am from Chittagong.
 Currently i am living Uttara Dhaka"
@@ -51,3 +58,27 @@ cat(str)
 str<-"We are the so called \"Vikings\",from the north."
 str
 cat(str)
+
+
+#Data input
+#readline() it is an string input function
+var1=readline(prompt = "Enter any  value: ")
+var2=readline(prompt = "Enter any number: ")
+var2=as.integer(var2)
+print(var1)
+print(var2)
+
+#sum two number
+var1=readline(prompt = "Enter any number: ")
+var2=readline(prompt = "Enter any number: ")
+var1=as.integer(var1)
+var2=as.integer(var2)
+print(var1+var2)
+
+var=readline()
+print(var)
+
+#data input using scan() method and also read a file
+x=scan()
+print(x)
+
